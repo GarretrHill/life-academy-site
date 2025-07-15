f3578be7cc27b42f79f9faa77d7b23dbf29ce207
Garret Hill <garrethill21@gmail.com>
2025-07-14 20:57:39 -0500

    Initial commit for Life Academy Website

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Placeholder for interactivity later
+console.log("Life Academy site loaded.");
+
+document.getElementById("menu-toggle").addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("active");
+});
